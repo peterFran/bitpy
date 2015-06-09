8ab63b8f7d464c157f83d340071509970c3fc30d
Peter Meckiffe <pmeckif1@oc6583033214.ibm.com>
2015-06-09 11:23:32 +0100

    restructured project

---
+++ modelling/fft_test.py
@@ -0,0 +1,28 @@
+__author__ = 'pmeckif1'
+import numpy as np
+import scipy as sy
+import scipy.fftpack as syfp
+import pylab as pyl
+import pickle
+
+# Read in data from file here
+array = np.loadtxt("data.csv")
+length = len(array)
+with open('stats_pickle', 'rb') as f:
+    mydata = pickle.load(open('stats_pickle', 'rb'))
+    print(mydata)
+    np_data = np.array(mydata)
+# Create time data for x axis based on array length
+x = sy.linspace(0.00001, length*0.00001, num=length)
+
+# Do FFT analysis of array
+FFT = sy.fft(array)
+# Getting the related frequencies
+freqs = syfp.fftfreq(array.size, d=(x[1]-x[0]))
+
+# Create subplot windows and show plot
+pyl.subplot(211)
+pyl.plot(x, array)
+pyl.subplot(212)
+pyl.plot(freqs, sy.log10(FFT), 'x')
+pyl.show()
